fdde8943981a29e771ce558ee508f7ba6281c553
= <tuannm@comartek.com>
2023-09-14 20:30:01 +0700

    new

--- Day6/level2/src/component/Number.js
+++ Day6/level2/src/component/Number.js
@@ -2,8 +2,25 @@ import React from "react";
 
 function Numbers({ numbers }) {
   const list = numbers.map((number) => {
-    let color = number % 2 === 0 ? "red" : "green";
+    let color = number % 2 === 0 ? "green" : "green";
+    let flag = true;
+    if (number < 2) {
+      flag = false;
+    } else if (number === 2) {
+      flag = true;
+    } else if (number % 2 === 0) {
+      flag = false;
+    } else {
+      for (var i = 3; i < number - 1; i += 2) {
+        if (number % i === 0) {
+          flag = false;
+        }
+      }
+    }
 
+    if (flag === true) {
+      document.getElementById(number).style.backgroundColor = "red";
+    }
     return (
       <div
         onClick={() => {
